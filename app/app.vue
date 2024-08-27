4a22b7c167f7887b40cf7afa6f31df9638cd71b1
Vitalii Rudenko <rvsrudik@gmail.com>
2024-08-27 10:29:20 -0600

    init project

---
+++ app/app.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="uppercase underline">nuxt app</div>
+</template>
---
+++ app/nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-04-03',
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  devtools: { enabled: true }
+  
+})
