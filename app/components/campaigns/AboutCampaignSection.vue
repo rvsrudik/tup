acb4d9ef78c81c652c0ba0788e63c39d79372fff
Vitalii Rudenko <rvsrudik@gmail.com>
2024-09-02 07:19:45 -0600

    feat: about campaign text

--- app/components/campaigns/AboutCampaignSection.vue
+++ app/components/campaigns/AboutCampaignSection.vue
@@ -6,10 +6,35 @@
       <GoalChart />
       <DonatedPeopleWidget />
     </div>
+
+    <div>
+      <h5 class="font-montserrat text-gray-900 mb-1">Campaign breakdown</h5>
+      <p class="text-sm text-slate-500">
+        Your donation will fund a job training coach, empowering abused women with the skills they need to secure
+        employment and rebuild their lives. By providing job training, we are helping women in our community transition
+        from crisis to stability.
+      </p>
+    </div>
+
+    <div>
+      <h5 class="font-montserrat text-gray-900">Town impact</h5>
+      <p class="text-sm text-slate-500">
+        This investment reduces unemployment and poverty rates in our town, leading to a stronger, more self-reliant
+        population.
+      </p>
+    </div>
+
+    <div class="flex gap-4 justify-end">
+      <AppButton size="xl" class=""
+        >Donate $ <img src="~/assets/icons/arrow-bold.svg" class="ml-3" alt="Arrow icon"
+      /></AppButton>
+      <AppButton size="xl" outline class="">Send a message</AppButton>
+    </div>
   </section>
 </template>
 
 <script setup>
 import GoalChart from '@/components/campaigns/GoalChart';
 import DonatedPeopleWidget from '@/components/campaigns/DonatedPeopleWidget';
+import AppButton from '@/components/ui/AppButton';
 </script>
