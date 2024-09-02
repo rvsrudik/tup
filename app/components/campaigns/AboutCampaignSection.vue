11a2f7c55ec72213d3112146898c7190444c968d
Vitalii Rudenko <rvsrudik@gmail.com>
2024-09-02 07:10:21 -0600

    feat: donated widget

--- app/components/campaigns/AboutCampaignSection.vue
+++ app/components/campaigns/AboutCampaignSection.vue
@@ -2,12 +2,14 @@
   <section class="bg-white grid gap-6 p-4 rounded-xl shadow-md">
     <h2 class="font-montserrat font-semibold text-2xl text-gray-800">About campaigh</h2>
 
-    <div class="flex flex-col md:flex-row md:justify-between">
+    <div class="flex flex-col md:flex-row md:justify-between gap-8">
       <GoalChart />
+      <DonatedPeopleWidget />
     </div>
   </section>
 </template>
 
 <script setup>
 import GoalChart from '@/components/campaigns/GoalChart';
+import DonatedPeopleWidget from '@/components/campaigns/DonatedPeopleWidget';
 </script>
---
+++ app/components/campaigns/DonatedPeopleWidget.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex flex-col md:flex-row md:items-center relative">
+    <!-- :style="{ transform: `translateX(${(imgSize * (images.length - 1)) / 2}px)` }" -->
+    <div class="relative flex md:translate-x-[125px]">
+      <img
+        v-for="(img, i) in images"
+        :key="i"
+        src="https://static.vecteezy.com/system/resources/previews/024/183/502/original/male-avatar-portrait-of-a-young-man-with-a-beard-illustration-of-male-character-in-modern-color-style-vector.jpg"
+        alt="Donated user avatar"
+        :width="imgSize"
+        :height="imgSize"
+        class="rounded-full"
+        :style="{ transform: `translateX(${i * -50}%)` }"
+      />
+    </div>
+
+    <div class="md:ml-6 mt-4 md:mt-0 font-montserrat">100+ people donated</div>
+  </div>
+</template>
+
+<script setup>
+const images = [0, 1, 2, 3, 4, 5];
+const imgSize = 50;
+</script>
