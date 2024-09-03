3c9f25dca2795dbf391dd705dd7d5f3453f0fec4
Yaroslav Bernatovych <108272773+yabr87@users.noreply.github.com>
2024-09-03 20:41:54 +0300

    chat start

---
+++ app/components/chat/ChatMyMessage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="relative border-t p-4 pt-6">
+    <div class="w-full md:w-[90%] ml-auto bg-purple-300 p-4 pr-8">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima facere
+      deleniti quae veniam aliquam voluptatibus qui fugiat aliquid labore optio
+      similique maxime velit doloremque, nostrum nulla placeat voluptatem natus
+      illum esse animi harum asperiores quo? Quisquam error sequi in ratione
+      nostrum voluptates numquam, dolor, atque maiores optio sit vitae incidunt.
+    </div>
+  </div>
+</template>
+
+<script setup></script>
---
+++ app/components/chat/ChatWindow.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex-grow overflow-y-auto p-4 rounded-2xl bg-white">
+    <ChatMyMessage></ChatMyMessage>
+  </div>
+</template>
+
+<script setup>
+import ChatMyMessage from './ChatMyMessage.vue';
+</script>
