1d590cda4aa36308dc2452a4dd0729b33669bb4d
Vitalii Rudenko <rvsrudik@gmail.com>
2024-09-03 10:54:58 -0600

    feat: header

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,6 +3,7 @@
     <AppSidebar />
     <div class="w-full">
       <div class="flex-grow py-10 pl-14 pr-2 md:pr-20 min-h-dvh w-full mx-auto max-w-screen-2xl">
+        <DefaultLayoutHeader />
         <slot class="mx-auto" />
       </div>
     </div>
@@ -11,4 +12,5 @@
 
 <script setup>
 import AppSidebar from '@/components/AppSidebar';
+import DefaultLayoutHeader from '@/components/DefaultLayoutHeader';
 </script>
