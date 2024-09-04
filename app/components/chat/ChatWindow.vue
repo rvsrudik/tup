dbc1b27bad00e40ce3208b6aac8ebce05b6d9afc
Yaroslav Bernatovych <108272773+yabr87@users.noreply.github.com>
2024-09-04 19:54:44 +0300

    add message component

--- app/components/chat/ChatWindow.vue
+++ app/components/chat/ChatWindow.vue
@@ -2,20 +2,51 @@
   <div
     class="custom-scrollbar flex-grow overflow-y-auto p-4 rounded-2xl bg-white"
   >
-    <ChatIncomingMessage />
-    <ChatMyMessage :isViewed="true" />
-    <ChatIncomingMessage />
-    <ChatMyMessage :isViewed="true" />
-    <ChatIncomingMessage />
-    <ChatMyMessage :isViewed="true" />
-    <ChatIncomingMessage />
-    <ChatMyMessage :isViewed="false" />
+    <!-- Перевірка стану завантаження даних -->
+    <div v-if="isLoading">Loading...</div>
+    <div v-else-if="error">Error loading data.</div>
+    <div v-else>
+      <ChatMessage
+        v-for="message in messages"
+        :key="message.id"
+        :name="message.name"
+        :date="message.date"
+        :text="message.text"
+        :avatar="message.avatar"
+        :files="message.files"
+        :isViewed="message.isViewed"
+      />
+    </div>
   </div>
 </template>
 
 <script setup>
-import ChatMyMessage from './ChatMyMessage.vue';
-import ChatIncomingMessage from './ChatIncomingMessage.vue';
+import ChatMessage from './ChatMessage.vue';
+
+const messages = ref([]);
+const isLoading = ref(true);
+const error = ref(null);
+
+const fetchMessages = async () => {
+  try {
+    const response = await fetch(
+      'https://66d86ce037b1cadd80549f70.mockapi.io/chat'
+    );
+    if (!response.ok) {
+      throw new Error('Failed to fetch messages');
+    }
+    const data = await response.json();
+    messages.value = data;
+  } catch (err) {
+    error.value = err.message;
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+onMounted(() => {
+  fetchMessages();
+});
 </script>
 
 <style scoped>
