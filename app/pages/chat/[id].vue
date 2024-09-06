ae26e70a9ac2a2f7c534986a29ac9d97a08a2eb9
Yaroslav Bernatovych <108272773+yabr87@users.noreply.github.com>
2024-09-06 13:12:04 +0300

    fix chat height

--- app/pages/chat/[id].vue
+++ app/pages/chat/[id].vue
@@ -1,8 +1,5 @@
 <template>
-  <!-- <div>hello</div>
-  {{ message }}
-  <AppButton @click="foo">add</AppButton> -->
-  <div class="flex flex-col gap-6 min-h-full h-full">
+  <div class="custom-height flex flex-col gap-6 h-full min-h-[390px]">
     <ChatWindow> </ChatWindow> <ChatInput />
   </div>
 </template>
@@ -10,9 +7,16 @@
 <script setup>
 import ChatWindow from '@/components/chat/ChatWindow';
 import ChatInput from '@/components/chat/ChatInput';
-
-// function foo() {
-//   message.value += 1;
-// }
-// const message = ref(1);
 </script>
+
+<style scoped>
+.custom-height {
+  max-height: calc(100dvh - 180px);
+}
+
+@media screen and (max-width: 480px) {
+  .custom-height {
+    max-height: calc(100dvh - 240px);
+  }
+}
+</style>
