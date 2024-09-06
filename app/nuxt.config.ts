36f0e9051b253c194b4ae86ee43fcf3a430e7198
Vitalii Rudenko <rvsrudik@gmail.com>
2024-09-06 06:34:10 -0600

    fix: dark mode issue

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   css: ['~/assets/css/main.css'],
+  colorMode: {
+    preference: 'light'
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
